2ceced7cd6c9ea3a923759de6cc812a01cf68998
Yan Li <eyeplum@gmail.com>
2023-01-10 21:57:51 +1300

    Initial commit

---
+++ tests/test_utf.lua
@@ -0,0 +1,17 @@
+local utf = loadfile('../utf.lua')()
+
+function test_single_u16_code_unit()
+    local code_units = utf.u32_to_u16(0x0020)
+    assert(#code_units, 1)
+    assert(code_units[1] == 0x0020)
+end
+
+function test_surrogate_pair()
+    local code_units = utf.u32_to_u16(0x1D405)
+    assert(#code_units, 2)
+    assert(code_units[1] == 0xD835)
+    assert(code_units[2] == 0xDC05)
+end
+
+test_single_u16_code_unit()
+test_surrogate_pair()
---
+++ utf.lua
@@ -0,0 +1,28 @@
+local utf = {}
+
+-- UTF-8 <=> UTF-32
+
+function utf.u8_to_u32(code_units)
+    -- TODO
+end
+
+function utf.u32_to_u8(code_point)
+    -- TODO
+end
+
+-- UTF-8 <=> UTF-16
+
+-- TODO: convenience function for UTF-8 => UTF-16
+-- TODO: convenience function for UTF-16 => UTF-8
+
+-- UTF-16 <=> UTF-32
+
+function utf.u16_to_u32(code_units)
+    -- TODO
+end
+
+function utf.u32_to_u16(code_point)
+    -- TODO
+end
+
+return utf
